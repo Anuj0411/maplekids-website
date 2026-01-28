ffe33a80c7347c8e1a38f4e6b05594f39410ddd5
Anuj0411 <anuj.parashar.140@gmail.com>
2026-01-28 10:31:41 +0530

    fix: build fixes

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,6 @@
 const CracoAlias = require('craco-alias');
 const path = require('path');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   plugins: [
@@ -27,6 +28,54 @@ module.exports = {
       '@/assets': path.resolve(__dirname, 'src/assets'),
       '@/pages': path.resolve(__dirname, 'src/pages'),
       '@/i18n': path.resolve(__dirname, 'src/i18n'),
+    },
+    configure: (webpackConfig, { env, paths }) => {
+      // Replace CSS minimizer with safer configuration
+      if (env === 'production') {
+        const minimizerIndex = webpackConfig.optimization.minimizer.findIndex(
+          (minimizer) => minimizer.constructor.name === 'CssMinimizerPlugin'
+        );
+        
+        if (minimizerIndex > -1) {
+          webpackConfig.optimization.minimizer[minimizerIndex] = new CssMinimizerPlugin({
+            minimizerOptions: {
+              preset: [
+                'default',
+                {
+                  discardComments: { removeAll: true },
+                  normalizeWhitespace: true,
+                  // Disable unsafe optimizations
+                  svgo: false,
+                  calc: false,
+                  colormin: false,
+                  convertValues: false,
+                  discardOverridden: false,
+                  mergeRules: false,
+                  minifyFontValues: false,
+                  minifyGradients: false,
+                  minifyParams: false,
+                  minifySelectors: false,
+                  normalizeCharset: false,
+                  normalizeDisplayValues: false,
+                  normalizePositions: false,
+                  normalizeRepeatStyle: false,
+                  normalizeString: false,
+                  normalizeTimingFunctions: false,
+                  normalizeUnicode: false,
+                  normalizeUrl: false,
+                  orderedValues: false,
+                  reduceIdents: false,
+                  reduceInitial: false,
+                  reduceTransforms: false,
+                  uniqueSelectors: false,
+                },
+              ],
+            },
+          });
+        }
+      }
+      
+      return webpackConfig;
     }
   }
 };
